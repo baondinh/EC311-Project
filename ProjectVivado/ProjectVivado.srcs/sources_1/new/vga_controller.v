797e52723465e17a1a0cd646f0d183e53fc6299d
Dinh <baondinh@bu.edu>
2024-11-26 12:52:05 -0500

    Vivado project folder and constraint file

---
+++ ProjectVivado/ProjectVivado.srcs/sources_1/new/vga_controller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/26/2024 12:50:49 PM
+// Design Name: 
+// Module Name: vga_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_controller(
+
+    );
+endmodule
